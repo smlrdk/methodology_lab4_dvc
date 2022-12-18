ea4d1a45494873a4b0abb59afb2d9af1b56902a6
Maria Solodkaya <solodkaya2013masha@gmail.com>
2022-12-18 20:10:59 +0300

    dvc2

--- evaluate.py
+++ evaluate.py
@@ -9,15 +9,15 @@ with open('k-means/predict.txt', 'r') as file:
     for line in file:
         kmeans_pred.append(int(line))
 
-# dtree_pred = list()
-# with open('dtree/predict.txt', 'r') as file:
-#     for line in file:
-#         dtree_pred.append(int(line))
+dtree_pred = list()
+with open('dtree/predict.txt', 'r') as file:
+    for line in file:
+        dtree_pred.append(int(line))
 
-# dtree_report = accuracy_score(y_test, dtree_pred)
+dtree_report = accuracy_score(y_test, dtree_pred)
 kmeans_report = accuracy_score(y_test, kmeans_pred)
 
 with open('metrics.txt', 'w') as file:
     file.write(f"k-means acc: {kmeans_report}")
-    # file.write("\n")
-    # file.write(f"dtree acc: {dtree_report}")
+    file.write("\n")
+    file.write(f"dtree acc: {dtree_report}")
